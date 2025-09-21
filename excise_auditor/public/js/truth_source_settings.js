a5bd1152add2d4e6ff55f27c493b401a0079b0c8
sdyankov <sdyankov@yahoo.com>
2025-09-21 20:40:07 +0300

    Create truth_source_settings.js

---
+++ excise_auditor/public/js/truth_source_settings.js
@@ -0,0 +1,10 @@
+frappe.ui.form.on('Truth Source Settings', {
+  refresh(frm) {
+    frm.add_custom_button(__('Sync Permissions Now'), () => {
+      frappe.call({
+        method: 'excise_auditor.excise_auditor.doctype.truth_source_settings.truth_source_settings.sync_permissions_now',
+        callback() { frappe.msgprint(__('Permissions synced')); }
+      });
+    });
+  }
+});
