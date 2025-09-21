573d60cf23fd569c587de8c1a34f4ff5ce4c8e1a
sdyankov <sdyankov@yahoo.com>
2025-09-21 20:53:43 +0300

    Update permission.py

--- excise_auditor/api/permission.py
+++ excise_auditor/api/permission.py
@@ -1,1 +1,6 @@
+import frappe
 
+def has_app_permission(*args, **kwargs):
+    """Tile visible for System Manager or Excise Auditor."""
+    roles = set(frappe.get_roles(frappe.session.user))
+    return bool({"System Manager", "Excise Auditor"} & roles)
