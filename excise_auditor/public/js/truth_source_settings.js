a27e0b93ce0f9f5d07ee36be6311c78749153d8a
Simeon Parvanov <sp.parvanov.gmail>
2025-12-30 17:45:14 +0200

    Fix duplicated module path in sync_permissions method call

--- excise_auditor/public/js/truth_source_settings.js
+++ excise_auditor/public/js/truth_source_settings.js
@@ -2,7 +2,7 @@ frappe.ui.form.on('Truth Source Settings', {
   refresh(frm) {
     frm.add_custom_button(__('Sync Permissions Now'), () => {
       frappe.call({
-        method: 'excise_auditor.excise_auditor.doctype.truth_source_settings.truth_source_settings.sync_permissions_now',
+        method: 'excise_auditor.doctype.truth_source_settings.truth_source_settings.sync_permissions_now',
         callback() { frappe.msgprint(__('Permissions synced')); }
       });
     });
